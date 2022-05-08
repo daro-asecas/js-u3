6d81e198c691baf482a8bdadf69c4d9e7e348b13
daro-asecas <daro.1919@gmail.com>
2022-05-08 14:52:28 -0300

    funcionando

--- control.js
+++ control.js
@@ -1,25 +1,24 @@
-function validateForm() {
+function validateForm(event) {
     let errores = [];
     let corregir = 0;
 
     let user = document.forms["login"]["user"].value;
     if (user == "") {
-        errores.push("No escribiste el usuario.");
-        corregir=1;
+        errores.push("No escribiste el usuario. Acordate que debe contener @.");
+        corregir++;
     }else if (!user.includes("@")) {
         errores.push("El usuario debe contener @.");
-        corregir=1;
+        corregir++;
     }
     let pass = document.forms["login"]["password"].value;
     if (pass == "") {
         errores.push("No escribiste la clave.");
-        corregir=1;
+        corregir++;
     }
 
-    if (corregir == 1) {
+    if (corregir > 0) {
         let erroresString = "\n \n" + errores.join("\n \n");
+        event.preventDefault();
         alert(erroresString);
-        alert(corregir);
-        return false;
     }
 }
